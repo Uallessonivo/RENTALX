7fdbd2e01eaa9b47286a2e237e5a84ba1c308b51
Uallesson Nunes Ivo <uallessons@gmail.com>
2021-11-20 07:33:59 -0300

    Autenticação e tokens do usuário

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRquest {
@@ -19,7 +20,7 @@ class CreateCategoryUseCase {
             await this.categoriesRepository.findByName(name);
 
         if (categoryAlreadyExists) {
-            throw new Error(`Category ${name} already exists`);
+            throw new AppError(`Category ${name} already exists`);
         }
 
         await this.categoriesRepository.create({ name, description });
