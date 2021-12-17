c1ba81f7ee66661032882320983dd03e04ab68a6
Uallesson Nunes Ivo <uallessons@gmail.com>
2021-12-16 21:05:23 -0300

    iniciando a implementação do refresh token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "d24bfe535d8fc4d5a3c4822692056806",
+    secret_refresh_token: "0eedc1a4f0f9b143893de779513f08e4",
+    expires_in_token: "15m",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_days: 30,
+};
